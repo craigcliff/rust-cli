a15117f9ef3b757852d93e4b2c0f6b554946211e
craigcliff <craigcliff@gmail.com>
2023-08-05 08:15:55 +0200

    Grouping config values using Struct

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use std::fs::File; // file handling
 use std::io::prelude::*; // useful traits for i/o
 
 fn main() {
-    //in cases where the desired function is nested in more than one module (std::env::args), bring the parent module into scope rather than the function
+    // in cases where the desired function is nested in more than one module (std::env::args), bring the parent module into scope rather than the function
     let args: Vec<String> = env::args().collect(); // the collect function needs to be annoted as it's not able to infer the kind of collection
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new(); // holds contents of file after creation
     f.read_to_string(&mut contents)
@@ -20,10 +20,16 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    // the program name takes up the 1st element in array, hence we index first argment and 2nd argments starting from 1.
-    let query = &args[1];
-    let filename = &args[2];
+// helps convey the meaning of of the group of data - helps make code understandable
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+// parse_config returns an instance of the Config struct
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone(); // we dont have to manage lifetimes of references by cloning, but the tradeoff is, it takes more tume and memory than storing a reference to string data
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
