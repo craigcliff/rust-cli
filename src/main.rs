bf34fa0300c29ad62cf2ed57d3fa750e4ea97ca5
craigcliff <craigcliff@gmail.com>
2023-08-05 08:04:44 +0200

    Extracting argument parser

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ fn main() {
     //in cases where the desired function is nested in more than one module (std::env::args), bring the parent module into scope rather than the function
     let args: Vec<String> = env::args().collect(); // the collect function needs to be annoted as it's not able to infer the kind of collection
 
-    // the program name takes up the 1st element in array, hence we index first argment and 2nd argments starting from 1.
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
     let mut f = File::open(filename).expect("file not found");
 
-    let mut contents = String::new();
+    let mut contents = String::new(); // holds contents of file after creation
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    // the program name takes up the 1st element in array, hence we index first argment and 2nd argments starting from 1.
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
